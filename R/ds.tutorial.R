9f99f674f503575dd991879a377d811c3f4dd12a
Bernd Heesen <60698023+bheesen@users.noreply.github.com>
2021-10-15 10:48:27 +0200

    Add files via upload

---
+++ R/ds.tutorial.R
@@ -0,0 +1,10 @@
+#' Function to start a tutorial accompanying the package "datascience" and the book "Data Science und Statistik mit R"
+#'
+#' @param name is the name of the tutorial
+#' @keywords tutorial
+#' @example ds.tutorial(name = "ds.syntax")
+#' @export
+ds.tutorial <- 
+  function(name) {
+    learnr::run_tutorial(name, package = "datascience")
+  }
