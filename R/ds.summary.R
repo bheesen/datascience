a433cd1ad80081eb5c0929a3ca725c0bd5e6f4f3
Bernd Heesen <60698023+bheesen@users.noreply.github.com>
2022-11-30 10:19:37 +0100

    Add files via upload

--- R/ds.summary.R
+++ R/ds.summary.R
@@ -51,23 +51,26 @@ function(variable, name="") {
     rownames(var.gesamt)<-c("n","na","maxanzahl","modus","median","mean","skewness","skewness.txt","kurtosis","kurtosis.txt","spannweite","min","max","var","sd")
     if (is.numeric(variable)){
       var.modus<-as.numeric(var.modus)
-      ggplot2::ggplot(var.df) + ggplot2::aes(x=variable) +
+      p1<-ggplot2::ggplot(var.df) + ggplot2::aes(x=variable) +
         ggplot2::labs(title="Histogramm",subtitle=name,x=NULL,y="Häufigkeit")+
         ggplot2::geom_histogram(col="white")+
-        geom_vline(aes(xintercept=var.median,color="median",linetype="median"), 
-                   size=1)+
-        geom_vline(aes(xintercept=var.mean,color="mean",linetype="mean"), 
-                   size=2)+
-        geom_vline(aes(xintercept=var.modus,color="modus",linetype="modus"), 
-                   size=1)+
-        scale_color_manual(name="Kennzahl", 
+        ggplot2::geom_vline(ggplot2::aes(xintercept=var.median,color="median",linetype="median"), 
+                            linewidth=1)+
+        ggplot2::geom_vline(ggplot2::aes(xintercept=var.mean,color="mean",linetype="mean"), 
+                            linewidth=2)+
+        ggplot2::geom_vline(ggplot2::aes(xintercept=var.modus,color="modus",linetype="modus"), 
+                            linewidth=1)+
+        ggplot2::scale_color_manual(name="Kennzahl", 
           values=c(mean="red",median="blue",mean="red",modus="darkgreen")) +
-        scale_linetype_manual(name="Kennzahl", 
+        ggplot2::scale_linetype_manual(name="Kennzahl", 
           values=c(mean="solid",median="dashed",modus="dotted"))
-      ggplot2::ggplot(var.df) + ggplot2::aes(x=variable) +                                
+       gridExtra::grid.arrange(p1,nrow=1,ncol=1)
+      p1<-ggplot2::ggplot(var.df) + ggplot2::aes(x=variable) +                                
         ggplot2::geom_boxplot()+ ggplot2::coord_flip()+                                
         ggplot2::labs(title="Box-Plot",subtitle=name,x=NULL,y=NULL,ylab=NULL)+
-        ggplot2::theme(axis.ticks.x=element_blank(),axis.text.x=element_blank())
+        ggplot2::theme(axis.ticks.x=ggplot2::element_blank(),
+                       axis.text.x=ggplot2::element_blank())
+      gridExtra::grid.arrange(p1,nrow=1,ncol=1)
     } else {
       graphics::par(oma=c(2,0,0,0))                                 # Platz für Labels
       graphics::barplot(table(variable), space = 0,                 # Space zwischen Balken
